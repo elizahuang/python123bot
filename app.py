b81b9bee526311a1773bd62efa9644cdb7432dc1
eliza huang <eliza85827@gmail.com>
2020-05-22 19:50:03 +0800

    ok

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
 from flask import Flask, request, abort
 from linebot import(LineBotApi, WebhookHandler)
 from linebot.exceptions import(InvalidSignatureError)
-from linebot.models import *  #need revise
+from linebot.models import (MessageEvent,FollowEvent, TextMessage, ImageMessage, TextSendMessage, ImageSendMessage)
 import configparser, json, codecs, emoji
 import flexMsgTest
 import requests
+import os
 
 app=Flask(__name__)
 config=configparser.ConfigParser()
-#config.readfp(codecs.open("config.ini", "r", "utf8"))
 config.read_file(codecs.open("config.ini", "r", "utf8"))
-#config.read('config.ini')
+#config.read_file('config.ini')
 
 #Channel Access Token
 line_bot_api=LineBotApi(config.get('line-bot','channel_access_token'))
 #Channel Secret
 handler=WebhookHandler(config.get('line-bot','channel_secret'))
 
+@app.route("/greetingPic")
+def returnGreetingPic():
+    #print(dir(os))
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    greetingPic = os.path.join(fileDir, 'greetingPic.png')
+    return greetingPic
 
 #監聽來自 /callback的Post request  #伺服器設置來接收line發送過來資訊的位置
-@app.route("/callback", methods=['GET','POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
     #get X-Line-Signature header value
     signature=request.headers['X-Line-Signature']
@@ -40,34 +46,75 @@ def callback():
 #處理訊息
 @handler.add(FollowEvent)
 def follow(event):
+    #savePatientInfo()  firstname, lastname, gender , card number, id number, chatbot number
+    userId=event.source.user_id ## 紀錄userId到DB
+    print(userId)
+    #getPatientInfo();  firstname, lastname, gender, chatbot number
+    #getPharmInfo();  Pharmacy name, Pharmacist name
+    lastName="汪";
+    gender="male"
+    if gender=="male":
+        title="先生"
+    else: title="小姐"
+
+    pharmacyName="亮亮藥局"
+    pharmacistName="王藥師"
+    
+    #greetImgUrl=config.get('urls','greet_img_url')
+    greetImgUrl=config.get('urls','server_path')+config.get('urls','greeting_pic_url')
+
+    followMsg=lastName+title+"您好，\n我是"+pharmacyName+"的"+pharmacistName+"。\n"+config.get('followMsg','greeting_msg')
+    line_bot_api.reply_message(event.reply_token,ImageSendMessage(
+        type="image",
+        original_content_url=greetImgUrl,
+        preview_image_url=greetImgUrl
+    ))
+    line_bot_api.push_message(event.source.user_id,TextSendMessage(followMsg))
+    line_bot_api.push_message(event.source.user_id,TextSendMessage(config.get('followMsg','instruc')))
+    
+    """
     #line_bot_api.broadcast(SendMessage(text='broadcast_test'),True)
     followMsg=config.get('followMsg','greeting_msg')+ emoji.emojize(":grinning_face_with_big_eyes:")+"\n"+config.get('followMsg','instruc')
     #print(type(config.get('msgWords','follow_msg')))
     line_bot_api.reply_message(event.reply_token,TextSendMessage(followMsg));
+    """
     #StickerSendMessage(package_id=1, sticker_id=2)
-    userId=event.source.user_id ## 紀錄userId到DB
-    print(userId)
+    
 
 @handler.add(MessageEvent ,message=[TextMessage,ImageMessage])#||ImageMessage
 def echo(event):
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         #print(event.message)
-        messageId=event.message.id
-        print(messageId)       
-        print(event.message.type)
+        #messageId=event.message.id
+        #print(messageId)       
+        #print(event.message.type)
         if event.message.type=="image":
-            url='https://api-data.line.me/v2/bot/message/%s/content'%(messageId)
-            botToken=config.get('line-bot','channel_access_token')
-            headers = {'Authorization' : 'Bearer %s'%(botToken)}
+            url='https://api-data.line.me/v2/bot/message/{imageMessageId}/content'.format(imageMessageId=event.message.id)
+            headers = {'Authorization' : 'Bearer {botToken}'.format(botToken=config.get('line-bot','channel_access_token'))}
             bytesPic=requests.get(url,headers=headers).content
             
             with open("test.png", 'wb+') as fd:
                 fd.write(bytesPic)
-                
+            
+            #save picbytes into file. Then open file and convert bytes in to image. 
+            """
+            with open('testWriteBytes', 'wb+') as fd:
+                fd.write(message_content)
+            with open('testWriteBytes', 'rb')as rd:
+                data=rd.read()
+            with open('test.png', 'wb+') as fd:
+                fd.write(data)
+            """
+
+            #Failed. Use line api to retreive message_content. 
+            '''
+            message_content = line_bot_api.get_message_content(event.message.id)
+            with open('test.png', 'wb+') as fd:
+                        fd.write(message_content)
+            '''                
         else:
             if event.message.text=="flexBubble":
                 content=flexMsgTest.returnBubble()
-                #replyMsg=FlexSendMessage(contents =get_or_new_from_json_dict_with_types(content))
                 replyMsg=FlexSendMessage(alt_text="flex test failed.",contents =content)
                 line_bot_api.reply_message(event.reply_token,replyMsg)
             elif event.message.text=="flexCarousel":
@@ -86,18 +133,12 @@ def echo(event):
                 line_bot_api.reply_message(event.reply_token, replyMsg)
                 line_bot_api.push_message(event.source.user_id, TextSendMessage(text='Hello World!'))
 
-
-            '''
-            message_content = line_bot_api.get_message_content(event.message.id)
-            with open('test.png', 'wb+') as fd:
-                        fd.write(message_content)
-            '''
             
 import os 
 if __name__=="__main__":
+    #app.debug=True
     port=int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0',port=port)
     app.run()
 
 
-####testing heroku git
